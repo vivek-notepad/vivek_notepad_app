018ee30ebbd54d0504f5baee92ee7fa8cbe23dbd
vivek-notepad <vvmh2014@gmail.com>
2025-07-17 11:57:34 +0530

    change in version for first prod build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.viveksingh.notepad_app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
