3fa9d179dbf200c648fc64616be75526b5cf23a1
vivek-notepad <vvmh2014@gmail.com>
2025-08-11 10:09:07 +0530

    new realease

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.viveksingh.notepad_app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
 
     signingConfigs {
